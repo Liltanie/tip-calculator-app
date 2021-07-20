f3e9677cd1cdf43e6eb5cee2acb0d931e18123a0
Candela Bramucci <candelabramucci05@gmail.com>
2021-07-20 13:27:45 -0300

    Lógica JS y pequeños cambios en index.html

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const bill = document.querySelector('.input--bill')
+const numberOfPeople = document.querySelector('.input--people')
+const tipAmount = document.querySelector('.tip-amount')
+const total = document.querySelector('.total')
+const reset = document.querySelector('.btn--reset')
+const buttons = document.querySelector('.grid-buttons')
+const customAmount = document.querySelector('.input--amount')
+const peopleError = document.querySelector('.people-error')
+let percent = 0;
+
+buttons.addEventListener('click', e => {
+    e.stopPropagation()
+    if (e.target.localName == 'button') percent = parseFloat(e.target.textContent)
+    validation()
+})
+
+customAmount.addEventListener('input', e => {
+    if (/\d+/.test(customAmount.value)) percent = parseFloat(customAmount.value)
+    validation()
+})
+
+numberOfPeople.addEventListener('input', e => {
+    validation()
+})
+
+bill.addEventListener('input', e => {
+    validation()
+})
+
+const validation = () => {
+    if (numberOfPeople.value != 0 && bill.value != ' ') {
+        showResult(numberOfPeople.value, bill.value, percent)
+        peopleError.textContent = ""
+    }
+    else showError()
+}
+
+const showResult = (people, bill, percent) => {
+    bill = parseFloat(bill), people = parseFloat(people)
+    if (isNaN(bill)) bill = 0 //para los casos en que bill es ''
+    let tip = (bill * percent) / 100
+    tipAmount.textContent = (tip / people).toFixed(2)
+    total.textContent = ((bill + tip) / people).toFixed(2)
+}
+
+const showError = () => {
+    peopleError.textContent = "Can't be zero"
+}
